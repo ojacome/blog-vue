eb5027dd35aac7c96060074c2ad74752c406cf21
olmedo <olmedo,bdp@gmail.com>
2020-11-09 18:04:52 -0500

    proyecto base

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Slider/>
+    <div class="center">
+      <LastArticles/>
+      <Sidebar/>
+    </div>
+    <div class="clearfix"></div>
+    <Footer/>
+  </div>
+</template>
+
+
+
+<script>
+import Header from './components/shared/Header.vue';
+import Slider from './components/shared/Slider.vue';
+import Sidebar from './components/shared/Sidebar.vue';
+import LastArticles from './components/LastArticles.vue';
+import Footer from './components/shared/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Slider,
+    Sidebar,
+    LastArticles,
+    Footer
+  }
+}
+</script>
+
+
+
+
+<style>
+@import './assets/css/style.css';
+</style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Este es mi componente</h1>
+        <h2>{{ subtitulo }}</h2>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name: 'MiComponente',
+    data(){
+        return {
+            subtitulo: 'Subtitulo desde otro componente'
+        }
+    }
+}
+</script>
